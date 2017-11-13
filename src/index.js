e14a8f913c12c6c12ab374047ad66202ecad0ba5
Aibol Kussain <aibolikdev@gmail.com>
2017-11-13 21:16:59 +0300

    submit: sumbit solution by Aibol Kussain

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function isPointsOnLine(points) {
-  // your solution here
+function isPointsOnLine(points) {
+  // Solution comes from counting Area by cartesian product
+  // If area is equal to 0, then all points lie on a single line.
+  if (points.length <= 2) return true;
+
+  var area = 0;
+
+  for (var i = 0; i < points.length - 1; i++) {
+    area = area + points[i][0] * points[i + 1][1] - points[i][1] * points[i + 1][0];
+  }
+  area += points[points.length - 1][0] * points[0][1] - points[points.length - 1][1] * points[0][0];
+
+  return area === 0;
 }
+
+module.exports = isPointsOnLine;
