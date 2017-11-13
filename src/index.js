74a5aa6e03cc30fde026d3e071306c5cdf486f93
Aibol Kussain <aibolikdev@gmail.com>
2017-11-13 21:21:14 +0300

    submit: attempt #2 to submit task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function isPointsOnLine(points) {
+module.exports = function isPointsOnLine(points) {
   // Solution comes from counting Area by cartesian product
   // If area is equal to 0, then all points lie on a single line.
   if (points.length <= 2) return true;
@@ -9,8 +9,7 @@ function isPointsOnLine(points) {
     area = area + points[i][0] * points[i + 1][1] - points[i][1] * points[i + 1][0];
   }
   area += points[points.length - 1][0] * points[0][1] - points[points.length - 1][1] * points[0][0];
+  area = Math.abs(area / 2);
 
   return area === 0;
 }
-
-module.exports = isPointsOnLine;
